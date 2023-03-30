4233013518a8430e7c20e18b5029dca1f7d3cbcc
amanda-vlopes <amandavitor@hotmail.com>
2023-03-30 15:51:40 -0300

    Requisito 8 finalizado

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        {}
+        <Title headline="Missões" />
       </div>
     );
   }
