66e14028f0210393f3f090ffea8112c0533a4d12
amanda-vlopes <amandavitor@hotmail.com>
2023-03-30 21:57:32 -0300

    Estilizando a pagina

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,11 +5,11 @@ export default class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">
+      <div data-testid="planet-card" className="solarSystem__card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } id={ planetName } />
+        <p data-testid="planet-name" className="planets__name">
           {planetName}
         </p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import './SolarSystem.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <Title headline="Planetas" />
-        {planets.map(({ name, image }) => (<PlanetCard
-          key={ name }
-          planetName={ name }
-          planetImage={ image }
-        />))}
+      <div data-testid="solar-system" className="solar__system">
+        <Title headline="PLANETAS" />
+        <div className="planets">
+          {planets.map(({ name, image }) => (<PlanetCard
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />))}
+        </div>
       </div>
     );
   }
